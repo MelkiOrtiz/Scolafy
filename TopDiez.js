dc0441800926b625235bad390a818549c7a5638b
Melki <mortizm28@miumg.edu.gt>
2025-02-21 19:54:18 -0600

    Finalizado

--- TopDiez.js
+++ TopDiez.js
@@ -65,7 +65,7 @@ class TopDiez {
             console.error(error);
         }
     }
-
+//
     async getTopTracks(accessToken) {
         const response = await fetch('https://api.spotify.com/v1/me/top/tracks?limit=10', {
             headers: { 'Authorization': 'Bearer ' + accessToken }
